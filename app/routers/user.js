a786dd354bb1b5272992cfc1bf8be81debad10e1
shanke <shanke@qunhemail.com>
2019-03-05 09:57:22 +0800

    修改route

--- app/routers/user.js
+++ app/routers/user.js
@@ -7,7 +7,7 @@ module.exports = app => {
     } = app;
     router.get('/users', controller.user.findAll);
     router.get('/user/:id', controller.user.findOne);
-    router.post('/update/:id', controller.user.update);
+    router.put('/user/:id', controller.user.update);
 
     // router.post('/user', controller.user.add);
     // router.del('/user/:id', controller.user.delete);
